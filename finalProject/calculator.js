d339c85bcb20809ddee454867bb28f3ddd924673
Frank Lai <frank810618@gmail.com>
2020-08-03 10:51:00 +0800

    half finished calculator

--- finalProject/calculator.js
+++ finalProject/calculator.js
@@ -1,9 +1,28 @@
 window.onload = () => {
     const numbers = document.querySelectorAll('.number');
+    const operators = document.querySelectorAll('.operator');
     const result = document.getElementById('result');
+    const acButton = document.getElementById('AC');
+    let calculateArr = [];
+    let operatorArr = [];
+    let isCalculating = false;
+    const records = [];
+    let currentNumber = 0;
 
-    document.getElementById('AC').onclick = () => {
+    const operation = {
+        '+': (x, y) => Number(x) + Number(y),
+        '-': (x, y) => Number(x) - Number(y),
+        'x': (x, y) => Number(x) * Number(y),
+        '÷': (x, y) => Number(x) / Number(y)
+    }
+
+    acButton.onclick = () => {
         result.innerHTML = '0';
+        acButton.innerHTML = 'AC';
+        calculateArr = [];
+        operatorArr = [];
+        isCalculating = false;
+        currentNumber = 0;
     }
 
     document.getElementById('negative').onclick = () => {
@@ -25,12 +44,35 @@ window.onload = () => {
 
     numbers.forEach(number => {
         let _self = number;
+
         _self.onclick = () => {
-            if (!Number(result.innerHTML) && result.innerHTML.indexOf('.') < 0) {
+
+            if (!Number(result.innerHTML) && result.innerHTML.indexOf('.') < 0 || isCalculating) {
                 result.innerHTML = _self.id;
             } else {
                 result.innerHTML += _self.id;
             }
+            currentNumber = Number(result.innerHTML);
+            acButton.innerHTML = Number(result.innerHTML) ? 'C' : 'AC';
         }
     });
+
+    operators.forEach(operator => {
+        operator.onclick = () => {
+            isCalculating = true;
+            calculateArr.push(result.innerHTML);
+            if (operatorArr.length) {
+                calculateArr.reduce((x, y, index) => {
+                    currentNumber = operation[operatorArr[index - 1]](x, y);
+                });
+                result.innerHTML = currentNumber;
+            }
+            operatorArr.push(operator.innerHTML);
+
+        }
+    });
+
+    document.getElementById('equal').onclick = () => {
+        result.innerHTML = operation[operatorArr[0]](currentNumber, result.innerHTML);
+    };
 }
