faafc7472ac0071501522c010000db93076b51a0
Frank Lai <frank810618@gmail.com>
2020-08-03 16:48:51 +0800

    finished final project

--- finalProject/calculator.js
+++ finalProject/calculator.js
@@ -1,21 +1,34 @@
+// 畫面載入
 window.onload = () => {
     new Calculator().init();
 }
 
+// 計算機主程式
 function Calculator() {
-
+    // 所有的數字按鈕DOM物件
     const numbers = document.querySelectorAll('.number');
+    // 加減乘除按鈕DOM物件
     const operators = document.querySelectorAll('.operator');
+    // 呈現結果的span DOM物件
     const result = document.getElementById('result');
+    // 清空鍵DOM物件
     const acButton = document.getElementById('AC');
+    // 計算紀錄清單DOM物件
     const recordsList = document.getElementById('recordList');
+    // 使用者輸入的數字
     let calculateArr = [];
+    // 使用者選的計算符
     let operatorArr = [];
+    // 是否為計算狀態
     let isCalculating = false;
+    // 是否為新數字
     let isNew = false;
+    // 計算紀錄
     const records = [];
+    // 當下數字
     let currentNumber = 0;
 
+    // 統一控管計算符
     const operation = {
         '+': (x, y) => Number(x) + Number(y),
         '-': (x, y) => Number(x) - Number(y),
@@ -23,7 +36,10 @@ function Calculator() {
         '÷': (x, y) => Number(x) / Number(y)
     }
 
+    // 初始
     this.init = () => {
+
+        // 清空
         acButton.onclick = () => {
             result.innerHTML = '0';
             acButton.innerHTML = 'AC';
@@ -33,6 +49,7 @@ function Calculator() {
             currentNumber = 0;
         }
 
+        // 正負號
         document.getElementById('negative').onclick = () => {
             let value = result.innerHTML;
             if (value.startsWith('-')) {
@@ -42,15 +59,18 @@ function Calculator() {
             }
         }
 
+        // 百分比
         document.getElementById('percent').onclick = () => {
             result.innerHTML = Number(result.innerHTML) / 100;
         }
 
+        // 小數點
         document.getElementById('point').onclick = () => {
             if (result.innerHTML.indexOf('.') < 0)
                 result.innerHTML += '.';
         }
 
+        // 數字按鍵事件註冊
         numbers.forEach(number => {
             let _self = number;
             _self.onclick = () => {
@@ -64,6 +84,7 @@ function Calculator() {
             }
         });
 
+        // 計算符按鍵事件註冊
         operators.forEach(operator => {
             operator.onclick = () => {
                 calculateArr.push(result.innerHTML);
@@ -86,11 +107,22 @@ function Calculator() {
             }
         });
 
+        // 等號按鈕事件註冊
         document.getElementById('equal').onclick = () => {
+            // 呈現結果
             calculateArr.push(result.innerHTML);
             const calResult = operation[operatorArr[operatorArr.length - 1]](currentNumber, result.innerHTML);
             result.innerHTML = calResult;
-            this.setRecords(calResult);
+
+            // 產生紀錄的DOM物件
+            let li = document.createElement('li');
+            li.className = 'list-group-item';
+            li.innerHTML = this.setRecord(calResult);
+
+            // 如果紀錄多於3筆?
+            records.push(li);
+
+            // 設定值還原
             isCalculating = false;
             isNew = true;
             calculateArr = [];
@@ -98,21 +130,26 @@ function Calculator() {
             currentNumber = 0;
         };
 
+        // 取得資料按鍵事件註冊
         document.getElementById('getRecord').onclick = () => {
-            console.log(this.getRecords()); // 取得所有紀錄的方法
             // 請由此開始作答
+
+            // 作法 1: 使用Promise
+            
+
+            // 作法 2: 使用async、await
+            
         };
     }
 
-    this.getRecords = () => records;
-
-    this.setRecords = (result) => {
+    // 算式字串串接
+    this.setRecord = (result) => {
         let record = '';
         calculateArr.forEach((cal, index) => {
             record = record + cal + ' ' + (operatorArr[index] ? operatorArr[index] + ' ' : ' ');
         });
         record = record + '= ' + result;
-        records.push(record);
+        return record;
     }
 
 }
