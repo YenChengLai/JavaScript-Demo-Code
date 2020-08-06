20fe35aa042111311345e45bdb089949dcf359ba
Frank Lai <frank810618@gmail.com>
2020-08-06 09:22:11 +0800

    remove .DS_Store

--- es6/class.js
+++ es6/class.js
@@ -1,11 +1,11 @@
 
 // 以函式建構式撰寫
-// function Person(name) {
-//     this.name = name;
-//     this.sayHi = () => {
-//         return `Hi, my name is ${name}.`;
-//     }
-// }
+function Person(name) {
+    this.name = name;
+    this.sayHi = () => {
+        return `Hi, my name is ${name}.`;
+    }
+}
 
 // function Student(name, studentMumber) {
 //     // 將this (此處為Student) 傳入Person執行，用以建立Person有的屬性
@@ -19,60 +19,57 @@
 
 
 // 以類別宣告
-class Person {
+// class Person {
 
-    constructor(name) {
-        this.name = name;
-    }
+//     constructor(name) {
+//         this.name = name;
+//         console.log('Hi');
+//     }
 
-    sayHi() {
-        return `Hi, my name is ${this.name}.`;
-    }
+//     sayHi() {
+//         return `Hi, my name is ${this.name}.`;
+//     }
 
-    static sayHello() {
-        return 'Hello';
-    }
-}
+//     static sayHello() {
+//         return 'Hello';
+//     }
+// }
 
-const teddy = new Person('Teddy');
-console.log(Person.sayHello());
-console.log(teddy.sayHello());
+// const teddy = new Person('Teddy');
+// console.log(Person.sayHello());
+// console.log(teddy.sayHello());
 
-class Student extends Person {
+// class Student extends Person {
 
-    constructor(name, studentNumber) {
-        super(name);
-        this.studentNumber = studentNumber;
-    }
+//     constructor(name, studentNumber) {
+//         super(name);
+//         this.studentNumber = studentNumber;
+//     }
 
-    getNumber = () => {
-        return `${this.name}'s student number is ${this.studentNumber}`;
-    }
+//     getNumber = () => {
+//         return `${this.name}'s student number is ${this.studentNumber}`;
+//     }
 
-    set age(age) {
-        console.log('value is set');
-        this._age = age;
-    }
+//     set age(age) {
+//         console.log('value is set');
+//         this._age = age;
+//     }
 
-    get age() {
-        if (!this._age) {
-            return 'not set yet.'
-        }
-        return this._age;
-    }
-}
-let Joe = new Student('Joe', 5);
-Joe.age = 20;
-console.log(Joe.age);
-console.log(Joe.getNumber());
-console.log(Joe.sayHi());
+//     get age() {
+//         if (!this._age) {
+//             return 'not set yet.'
+//         }
+//         return this._age;
+//     }
+// }
+// let Joe = new Student('Joe', 5);
+// Joe.age = 20;
+// console.log(Joe.age);
+// console.log(Joe.getNumber());
+// console.log(Joe.sayHi());
 
-// let john = new Person('John');
-// console.log(john);
-// console.log(john.sayHi());
-// let peter = new Person('Peter');
-// console.log(peter.sayHi === john.sayHi);
-
-
-
-
+let john = new Person('John');
+console.log(john);
+console.log(john.sayHi());
+let peter = new Person('Peter');
+console.log(peter.sayHi === john.sayHi);
